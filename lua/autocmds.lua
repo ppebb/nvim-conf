437078fed6c23b97e35ab5e636b58bd3f9c58ace
ppeb <ppeb@ppeb.me>
2023-01-07 22:01:04 -0600

    fix autocommands 💀

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -9,6 +9,16 @@ function Kill_EslintD()
     }):start()
 end
 
+function Is_Attached(bufnr)
+    local lsp = rawget(vim, "lsp")
+    if lsp then
+        for _, _ in pairs(lsp.buf_get_clients(bufnr)) do
+            return true
+        end
+    end
+    return false
+end
+
 function M.load()
     vim.cmd([[
         augroup highlight_yank
@@ -18,19 +28,13 @@ function M.load()
 
         augroup hover
             autocmd!
-            au CursorHold * silent! lua if is_attached(0) then if #vim.diagnostic.get(0) > 0 then vim.diagnostic.open_float(nil, {focus = false, focusable = false, scope = 'cursor'}) else vim.lsp.buf.hover(nil, { focus = false, focusable = false }) end end
+            au CursorHold * silent! lua if Is_Attached(0) then if #vim.diagnostic.get(0) > 0 then vim.diagnostic.open_float(nil, {focus = false, focusable = false, scope = 'cursor'}) else vim.lsp.buf.hover(nil, { focus = false, focusable = false }) end end
         augroup END
 
-        augroup Vimspector
+        augroup session
             autocmd!
-            au VimEnter * VimspectorLoadSession ~/.cache/vimspector
-            au VimLeave * VimspectorMkSession ~/.cache/vimspector
-        augroup END
-
-        " auto_start in settings broke for some reason
-        augroup coq
-            autocmd!
-            au VimEnter * COQnow --shut-up
+            au VimEnter * silent! VimspectorLoadSession ~/.cache/vimspector
+            au VimLeave * silent! VimspectorMkSession ~/.cache/vimspector
         augroup END
 
         augroup italics
