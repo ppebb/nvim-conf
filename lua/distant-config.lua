7ce9d30821229223b264a568f82d6c6d6d920ad9
ppeb <36719558+ppebb@users.noreply.github.com>
2023-10-11 00:00:57 +0000

    distant config

--- lua/distant-config.lua
+++ lua/distant-config.lua
@@ -3,40 +3,42 @@ local M = {}
 function M.config()
     local actions = require("distant.nav.actions")
     local cfg = {
-        ["*"] = {
-            file = {
-                mappings = {
-                    ["-"] = actions.up,
+        servers = {
+            ["*"] = {
+                file = {
+                    mappings = {
+                        ["-"] = actions.up,
+                    },
+                },
+                dir = {
+                    mappings = {
+                        ["<Return>"] = actions.edit,
+                        ["-"] = actions.up,
+                        ["K"] = actions.mkdir,
+                        ["N"] = actions.newfile,
+                        ["R"] = actions.rename,
+                        ["D"] = actions.remove,
+                    },
                 },
             },
-            dir = {
-                mappings = {
-                    ["<Return>"] = actions.edit,
-                    ["-"] = actions.up,
-                    ["K"] = actions.mkdir,
-                    ["N"] = actions.newfile,
-                    ["R"] = actions.rename,
-                    ["D"] = actions.remove,
+            [""] = {
+                mode = "ssh",
+                ssh = {
+                    user = "",
+                    identity_files = { "" },
                 },
-            },
-        },
-        [""] = {
-            mode = "ssh",
-            ssh = {
-                user = "",
-                identity_files = { "" },
-            },
-            lsp = {
-                [""] = {
-                    cmd = { "sourcekit-lsp" },
-                    filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp" },
-                    root_dir = "",
+                lsp = {
+                    [""] = {
+                        cmd = { "sourcekit-lsp" },
+                        filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp" },
+                        root_dir = "",
+                    },
                 },
             },
         },
     }
 
-    require("distant").setup(cfg)
+    require("distant"):setup(cfg)
 end
 
 return M
