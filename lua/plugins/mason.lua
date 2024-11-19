c7ae33ae4191eb2bb2957f7054313902bad02e2f
ppeb <ppeb@ppeb.me>
2024-11-18 20:52:36 -0600

    Add custom registry to mason for roslyn and rzls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,7 +2,12 @@ return {
     "williamboman/mason.nvim", -- Package manager for tools
     requires = { "williamboman/mason-lspconfig.nvim" },
     config = function()
-        require("mason").setup()
+        require("mason").setup({
+            registries = {
+                "github:mason-org/mason-registry",
+                "github:Crashdummyy/mason-registry",
+            },
+        })
         require("mason-lspconfig").setup()
     end,
 }
