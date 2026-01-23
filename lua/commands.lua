8a618db7cdfdf755594e9d61da7a95b092dda632
ppebb <ppeb@ppeb.me>
2026-01-22 20:35:41 -0600

    Add light mode command

--- lua/commands.lua
+++ lua/commands.lua
@@ -4,3 +4,8 @@ local yabs = require("yabs")
 api.nvim_create_user_command("Build", function() yabs:run_task("build") end, {})
 
 api.nvim_create_user_command("BuildAndRun", function() yabs:run_task("build_and_run") end, {})
+
+api.nvim_create_user_command("LightMode", function()
+    vim.cmd("colorscheme catppuccin-latte")
+    vim.cmd("hi CursorColumn guibg=#e9ebf1")
+end, {})
