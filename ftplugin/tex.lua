741e9f645f2565e4c3970881077cef10ef6ad560
ppeb <ppeb@ppeb.me>
2026-02-10 11:52:45 -0600

    Remove tex autocmds

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -9,13 +9,41 @@ local vimtex_ftplugin = vim.fn.stdpath("data") .. "/site/pack/pckr/opt/vimtex/ft
 
 vim.cmd("source " .. vimtex_ftplugin)
 
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--     pattern = "*.tex",
+--     callback = function(args)
+--         local Job = require("plenary.job")
+
+--         Job:new({
+--             command = "tex-fmt",
+--             args = { args.file },
+--         }):sync()
+
+--         vim.cmd("checktime")
+--     end,
+-- })
+
 vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = "*.tex",
     callback = function() vim.cmd(":VimtexCompile") end,
 })
 
-vim.api.nvim_create_autocmd("TextChanged", {
+-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+--     pattern = "*.tex",
+--     nested = true,
+--     callback = function() vim.cmd(":write") end,
+-- })
+
+-- TODO: Make this work
+vim.api.nvim_create_autocmd("VimLeavePre", {
     pattern = "*.tex",
-    nested = true,
-    callback = function() vim.cmd(":write") end,
+    callback = function()
+        local Job = require("plenary.job")
+
+        Job:new({
+            command = "bash",
+            args = { "-c", "'if pidof zathura > /dev/null; then; killall zathura; fi'" },
+            cwd = "/usr/bin",
+        }):sync() -- or start()
+    end,
 })
