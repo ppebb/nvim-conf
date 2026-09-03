65356adae6be9fc6538a01fcb635f405619b3353
ppebb <ppeb@ppeb.me>
2026-09-03 17:02:26 -0500

    Vimtex ignore under/overfull

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,5 +3,9 @@ return {
     config_pre = function()
         vim.g.vimtex_view_method = "zathura"
         vim.g.vimtex_compiler_method = "latexrun"
+        vim.g.vimtex_quickfix_ignore_filters = {
+            "Underfull",
+            "Overfull",
+        }
     end,
 }
