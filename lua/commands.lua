479efa56bc95991943f650b022cad78daf50c6ea
ppebb <ppeb@ppeb.me>
2026-09-06 19:56:32 -0500

    Fix lualine using the wrong color under light-mode

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local yabs = require("yabs")
+local lualine = require("lualine")
 
 api.nvim_create_user_command("Build", function() yabs:run_task("build") end, {})
 
@@ -8,6 +9,14 @@ api.nvim_create_user_command("BuildAndRun", function() yabs:run_task("build_and_
 api.nvim_create_user_command("LightMode", function()
     vim.cmd("colorscheme catppuccin-latte")
     vim.cmd("hi CursorColumn guibg=#e9ebf1")
+    local lualine_config = lualine.get_config()
+    lualine_config.options.theme = "catppuccin-latte"
+    lualine.setup(lualine_config)
 end, {})
 
-api.nvim_create_user_command("DarkMode", function() vim.cmd("colorscheme catppuccin-mocha") end, {})
+api.nvim_create_user_command("DarkMode", function()
+    vim.cmd("colorscheme catppuccin-mocha")
+    local lualine_config = lualine.get_config()
+    lualine_config.options.theme = "catppuccin-mocha"
+    lualine.setup(lualine_config)
+end, {})
